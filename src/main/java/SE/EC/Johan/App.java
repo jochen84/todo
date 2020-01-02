fae7cf6a84499418dc2318ced91c7b6ce5331352
jochen84 <simman@rocketmail.com>
2020-01-02 14:08:39 +0100

    First commit

---
+++ src/main/java/SE/EC/Johan/App.java
@@ -0,0 +1,13 @@
+package SE.EC.Johan;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
